7fa85697b2eb29362dab0ab36b69b794f8dc2859
Alexander Lozovoy <berpress@gmail.com>
2021-11-22 20:19:44 +0300

    Add shop

--- src/components/GoodsItems.jsx
+++ src/components/GoodsItems.jsx
@@ -1,21 +1,40 @@
 function GoodsItem(props) {
-  const {description, image, price, itemId, name} = props;
+  const {description, 
+          image, 
+          price, 
+          itemID, 
+          name, 
+          addToBasket = Function.prototype, 
+        } = props;
 
   return (
-    <div className="card" id={itemId}>
-      <div className="card-image">
-        <img src={image} alt={name}/>
+    <div className='card'>
+        <div className='card-image'>
+            <img src={image} alt={name} />
         </div>
-      <div className="card-content">
-        <span className="card-title">{name}</span>
-        <p>{description}</p>
-      </div>
-      <div className="card-action">
-          <button className='btn'>Buy</button>
-          <span className="right" style={{ fontSize: '1.8rem' }}>{price} RUB</span>
-      </div>
+        <div className='card-content'>
+            <span className='card-title'>{name}</span>
+            <p>{description}</p>
+        </div>
+        <div className='card-action'>
+            <button
+                className='btn'
+                onClick={() =>
+                    addToBasket({
+                        itemID,
+                        name,
+                        price,
+                    })
+                }
+            >
+                Buy
+            </button>
+            <span className='right' style={{ fontSize: '1.8rem' }}>
+                {price} ₽
+            </span>
+        </div>
     </div>
-   )
+);
 }
 
 export { GoodsItem };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,62 @@
 import React, { Component, useState, useEffect } from 'react';
+import { API_URL } from '../config.js'
+
 
 function Header() {
+  const [balance, setUserBalance] = useState(0);
+  
+  useEffect(() => {
+    async function fetchMyAPI() {
+      const email = `${Date.now()}_admin.com`;
+      const password = 'Password11';
+      console.log(email);
+      // register
+      const registerData = {
+        username: email,
+        password: password
+      }
+      const response = await fetch(`${API_URL}/register`, { 
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(registerData)
+      });
+      const data = await response.json();
+      const uuid = data.uuid;
+      console.log(`Get ${uuid}`);
+      // Auth and get token
+      const responseAuth = await fetch(`${API_URL}/auth`, { 
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(registerData)
+      });
+      const dataAuth = await responseAuth.json();
+      const token = dataAuth.access_token;
+      console.log(`Get token ${token}`);
+      // Set user balance
+      const registerBalance = {
+        balance: 10000,
+      }
+      const responseBalance = await fetch(`${API_URL}/balance/${uuid}`, { 
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',
+                  "Authorization": `JWT ${token}`},
+        body: JSON.stringify(registerBalance)
+      });
+      const dataBalance = await responseBalance.json();
+      const balance = dataBalance.balance;
+      console.log(`Balance is ${balance}`);
+      setUserBalance(balance);
+    }
+
+    fetchMyAPI()
+  }, [])
+
+  // const setBalance = (money) => {
+  //   const newBalance = balance - money;
+  //   setUserBalance(newBalance);
+  // }
+
+
   return (
       <nav className='darken-1'>
           <div className='nav-wrapper'>
@@ -14,7 +70,27 @@ function Header() {
                           target='_blank'
                           rel='noreferrer'
                       >
-                          Repo
+                          GitHub
+                      </a>
+                  </li>
+
+                  <li>
+                      <a
+                          href='https://github.com/berpress'
+                          target='_blank'
+                          rel='noreferrer'
+                      >
+                          Balance is {balance}
+                      </a>
+                  </li>
+
+                  <li>
+                      <a
+                          href='https://github.com/berpress'
+                          target='_blank'
+                          rel='noreferrer'
+                      >
+                          Login
                       </a>
                   </li>
               </ul>
